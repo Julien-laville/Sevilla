0546191cb6de978bcaf1e71991147ce37eaa1d39
Julien-laville <alternative451@myopera.com>
2016-10-15 16:14:17 +0200

    ipv4

--- ajaj.js
+++ ajaj.js
@@ -1,7 +1,7 @@
 function getAll() {
     var req = new XMLHttpRequest()
 
-    req.open('GET', "2a01:cb1d:2f5:bd00:416b:68d2:681d:10a2:8083", true);
+    req.open('GET', "90.116.73.55:8083", true);
     req.onreadystatechange = function (aEvt) {
         if (req.readyState == 4) {
             if(req.status == 200)
