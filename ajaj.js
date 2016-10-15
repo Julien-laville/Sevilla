ba399f99af6652f1cd92f3b6d329c3573d90f715
Julien-laville <alternative451@myopera.com>
2016-10-15 16:17:30 +0200

    add root url

--- ajaj.js
+++ ajaj.js
@@ -1,7 +1,7 @@
 function getAll() {
     var req = new XMLHttpRequest()
 
-    req.open('GET', "90.116.73.55:8083", true);
+    req.open('GET', "http://90.116.73.55:8083", true);
     req.onreadystatechange = function (aEvt) {
         if (req.readyState == 4) {
             if(req.status == 200)
