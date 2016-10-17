ccc05246cb0511fd1032c6b972d2f363db6590c4
julien.l <julien.l@qwant.com>
2016-10-17 17:03:01 +0200

    moar function

--- nano-host/index.js
+++ nano-host/index.js
@@ -1,8 +1,10 @@
 var http = require('http')
 var sqlite3 = require('sqlite3')
-
+var path = require('path')
 var url = require('url');
 
+
+var dbPath = parh.resolve(__dirname, "/kvs.db")
 
 var nanoServ = http.createServer(function (req, res) {
     var reqUrl = url.parse(req.url, true)
@@ -25,7 +27,7 @@ var nanoServ = http.createServer(function (req, res) {
 
 function getAll(res) {
     var allValues = [];
-    var dbkv = new sqlite3.Database('nano-host/kvs.db');
+    var dbkv = new sqlite3.Database(dbPath);
     dbkv.all("SELECT * from key_values", function (error, rows) {
         if(error) {
             res.writeHead('500');
@@ -40,6 +42,8 @@ function getAll(res) {
 }
 
 function cleanAll(res) {
+    var dbkv = new sqlite3.Database(dbPath);
+
    dbkv.exec("DELETE from key_values", function(error) {
        if(error) {
             res.writeHead('500');
@@ -47,14 +51,38 @@ function cleanAll(res) {
         } else {
             res.end(JSON.stringify({status : 'success'}))
         }
+        dbkv.close()
    }) 
 }
 
 function get(res, id) {
+    var dbkv = new sqlite3.Database(dbPath);
+    dbkv.get("SELECT * from key_values WHERE key = ?",{1 : id}, function(error, row) {
+        if(error) {
+            res.writeHead('500');
+            res.end('<h1><center>500</center></h1>' + error.toString())
+        } else {
+            res.end(JSON.stringify({value : row}))
+        }
+        dbkv.close()
+    })
     
+    
+
 }
 
 function put(res, key, value) {
+    var dbkv = new sqlite3.Database(dbPath);
+    
+    dbkv.run("INSERT into key_values (key, value) VALUES(?,?)", {1:key, 2:value}, function(error) {
+        if(error) {
+            res.writeHead('500');
+            res.end('<h1><center>500</center></h1>' + error.toString())
+        } else {
+            res.end(JSON.stringify({status : 'success'}))
+        }
+        dbkv.close()
+    })
     
 }
 
