6ec541163031aded7ff3cd1e1de340c47682eb29
Julien-laville <alternative451@myopera.com>
2016-10-17 21:17:05 +0200

    mini serv v1

--- nano-host/index.js
+++ nano-host/index.js
@@ -2,21 +2,23 @@ var http = require('http')
 var sqlite3 = require('sqlite3')
 var path = require('path')
 var url = require('url');
+var querystring = require('querystring');
 
-
-var dbPath = parh.resolve(__dirname, "/kvs.db")
+var dbPath = path.join(__dirname, "kvs.db")
 
 var nanoServ = http.createServer(function (req, res) {
     var reqUrl = url.parse(req.url, true)
 
     res.setHeader('Access-Control-Allow-Headers', 'julien-laville.github.io');
 
-    if(reqUrl.path === '/') {
-        getAll(res)
+    if(req.method === 'POST') {
+        put(res, req)
     } else if(reqUrl.query.id) {
         get(res,reqUrl.query.id)
     } else if(reqUrl.path === '/delete') {
         cleanAll(res)
+    } else if(reqUrl.path === '/') {
+        getAll(res)
     } else {
         res.writeHead(404);
         res.end('<h1><center>404</center></h1>')
@@ -62,7 +64,7 @@ function get(res, id) {
             res.writeHead('500');
             res.end('<h1><center>500</center></h1>' + error.toString())
         } else {
-            res.end(JSON.stringify({value : row}))
+            res.end(JSON.stringify({value : row.value}))
         }
         dbkv.close()
     })
@@ -71,18 +73,29 @@ function get(res, id) {
 
 }
 
-function put(res, key, value) {
+function put(res, req) {
     var dbkv = new sqlite3.Database(dbPath);
-    
-    dbkv.run("INSERT into key_values (key, value) VALUES(?,?)", {1:key, 2:value}, function(error) {
-        if(error) {
-            res.writeHead('500');
-            res.end('<h1><center>500</center></h1>' + error.toString())
-        } else {
-            res.end(JSON.stringify({status : 'success'}))
-        }
-        dbkv.close()
+    var fullBody = '';
+
+    req.on('data', function(chunk) {
+        // append the current chunk of data to the fullBody variable
+        fullBody += chunk.toString();
+    });
+    req.on('end', function() {
+
+        var decodedBody = querystring.parse(fullBody);
+
+        dbkv.run("INSERT into key_values (key, value) VALUES(?,?)", {1:decodedBody.key, 2:decodedBody.value}, function(error) {
+            if(error) {
+                res.writeHead('500');
+                res.end('<h1><center>500</center></h1>' + error.toString())
+            } else {
+                res.end(JSON.stringify({status : 'success'}))
+            }
+            dbkv.close()
+        })
     })
+
     
 }
 
